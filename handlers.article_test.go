b209c51cb6853b552716ff2e85e0c646f493431d
parul-123 <parulever333@gmail.com>
2020-03-17 16:57:16 +0530

    Adding simple article web app

---
+++ handlers.article_test.go
@@ -0,0 +1,60 @@
+// handlers.article_test.go
+
+package main
+
+import (
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+// Test that a GET request to the home page returns the home page with
+// the HTTP code 200 for an unauthenticated user
+func TestShowIndexPageUnauthenticated(t *testing.T) {
+	r := getRouter(true)
+
+	r.GET("/", showIndexPage)
+
+	// Create a request to send to the above route
+	req, _ := http.NewRequest("GET", "/", nil)
+
+	testHttpResponse(t, r, req, func(w *httptest.ResponseRecorder) bool {
+		// Test that the http status code is 200
+		statusOK := w.Code == http.StatusOK
+
+		// Test that the page title is "Home Page"
+		// You can carry out a lot more detailed tests using libraries that can
+		// parse and process HTML pages
+		p, err := ioutil.ReadAll(w.Body)
+		pageOK := err == nil && strings.Index(string(p), "<title>Home Page</title>") > 0
+
+		return statusOK && pageOK
+	})
+}
+
+// Test that a GET request to an article page returns the article page with
+// the HTTP code 200 for an unauthenticated user
+func TestArticleUnauthenticated(t *testing.T) {
+	r := getRouter(true)
+
+	// Define the route similar to its definition in the routes file
+	r.GET("/article/view/:artice_id", getArticle)
+
+	// Create a request to send to the above route
+	req, _ := http.NewRequest("GET", "/article/view/1", nil)
+
+	testHttpResponse(t, r, req, func(w *httptest.ResponseRecorder) bool {
+		// Test that the http status code is 200
+		statusOK := w.Code == http.StatusOK
+
+		// Test that the page title is "Article 1"
+		// You can carry out a lot more detailed tests using libraries that can
+		// parse and process HTML pages
+		p, err := ioutil.ReadAll(w.Body)
+		pageOK := err == nil && strings.Index(string(p), "<title>Article 1</title>") > 0
+
+		return statusOK && pageOK
+	})
+}
